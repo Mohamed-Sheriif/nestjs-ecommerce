06a57e91859719c4c0769011771435da65fe074c
Mohamed Sherif <mohamedsh2782@gmail.com>
2025-08-13 17:46:01 +0300

    implement userSchema

--- src/user/user.schema.ts
+++ src/user/user.schema.ts
@@ -5,8 +5,73 @@ export type UserDocument = HydratedDocument<User>;
 
 @Schema({ timestamps: true })
 export class User {
-  @Prop()
+  @Prop({
+    required: true,
+    type: String,
+    min: [3, 'Name must be at lesat 3 characters'],
+    max: [30, 'Name must be at lesat 30 characters'],
+  })
   name: string;
+
+  @Prop({
+    required: true,
+    type: String,
+    unique: true,
+  })
+  email: string;
+
+  @Prop({
+    required: true,
+    type: String,
+    min: [3, 'Password must be at lesat 3 characters'],
+    max: [20, 'Password must be at lesat 20 characters'],
+  })
+  password: string;
+
+  @Prop({
+    required: true,
+    type: String,
+    enum: ['user', 'admin'],
+    default: 'user',
+  })
+  role: string;
+
+  @Prop({
+    type: String,
+  })
+  avatar: string;
+
+  @Prop({
+    type: Number,
+  })
+  age: number;
+
+  @Prop({
+    type: String,
+  })
+  phoneNumber: string;
+
+  @Prop({
+    type: String,
+  })
+  address: string;
+
+  @Prop({
+    type: Boolean,
+    default: true,
+  })
+  active: boolean;
+
+  @Prop({
+    type: String,
+  })
+  validationCode: string;
+
+  @Prop({
+    type: String,
+    enum: ['male', 'female'],
+  })
+  gender: string;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
